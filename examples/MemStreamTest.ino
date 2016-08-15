28614ccb125ea8e377f75674027977661f05f708
Ingo Fischer <github@fischer-ka.de>
2016-08-15 09:49:44 +0200

    Update

--- examples/MemStreamTest.ino
+++ examples/MemStreamTest.ino
@@ -5,10 +5,12 @@ void setup() {
   Serial.begin(9600);
   Serial.println();
   Serial.println("START");
-  // put your setup code here, to run once:
   unsigned char buf[100] = "0123456789ABCDEFGHIJKLMNOPQRST";
+  // Initialize instance with a 100 byte buffer and 30 byte of available content, not writable
   MemStream testStream(buf,100,30,false);
+  // add three more characters
   testStream.write("XYZ");
+  // print out all available characters
   while (testStream.available()) {
     Serial.print("Avail: ");
     Serial.print(testStream.available());
@@ -24,8 +26,11 @@ void setup() {
   Serial.println();
   Serial.println();
 
+  // reset cursor position to 0
   testStream.flush();
+  // write some data
   testStream.write("UVW123456");
+  // print out all available characters
   while (testStream.available()) {
     Serial.print("Avail: ");
     Serial.print(testStream.available());
@@ -41,6 +46,4 @@ Serial.println("DONE");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
 }
